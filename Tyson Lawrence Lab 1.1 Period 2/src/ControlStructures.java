1f13810ba3e9af829d686a102d923d5fdbd7c29f
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-09-28 09:23:58 -0400

    Initializing Repository

---
+++ Tyson Lawrence Lab 1.1 Period 2/src/ControlStructures.java
@@ -0,0 +1,65 @@
+ 
+public class ControlStructures {
+//Tyson Lawrence Control Structures Period 2
+	public static void main(String[] args) 
+	{PrimePrinter();fooBarBaz();
+		
+		
+		
+		
+		
+	}
+	   
+	public static void PrimePrinter (int x, int b)
+    {   
+		{while (b>=1 && b<=10)
+		System.out.println (isPrime);
+		}
+		{while (b>=10 && b<=20)
+	     System.out.println((isPrime);
+		}
+		{while b>=20 && b<=30
+		}
+		
+	}
+	
+	public static boolean isPrime(int x)
+	{      	
+        {int number;int z;
+         for (z = 2;2*z<=number;z++)
+         {if (number%z==0)
+         {return false;}
+         } return true;
+    }
+			
+    }
+	public static int PrimeCount (int b)
+	{while (b>=1 && b<=10)
+	 {return b;}
+	}
+		
+	
+    public static String fooBarBaz (int a)
+    { for (int a1=0;a1<=500;a1++)
+      {String printout=""; 
+      if (a1%3==0)
+      {return printout+"foo";}
+      if (a1%7==0)
+      {return printout+"Bar";}
+      if (a1%10==0)
+      {return printout+"Baz";}
+      {System.out.println(printout);}    
+    }
+    	
+		   
+	      
+	     
+	      
+    }
+    }
+	      
+	    
+	
+         
+
+
