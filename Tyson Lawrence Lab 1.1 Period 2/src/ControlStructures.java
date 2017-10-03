d30f9de5c88e2ce8ed54e9440f25736513114540
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-10-03 09:17:28 -0400

    Added Print Codes

--- Tyson Lawrence Lab 1.1 Period 2/src/ControlStructures.java
+++ Tyson Lawrence Lab 1.1 Period 2/src/ControlStructures.java
@@ -2,7 +2,7 @@
 public class ControlStructures {
 //Tyson Lawrence Control Structures Period 2
 	public static void main(String[] args) 
-	{PrimePrinter();fooBarBaz();
+	{PrimePrinter();fooBarBazPrint();
 		
 		
 		
@@ -10,17 +10,19 @@ public class ControlStructures {
 		
 	}
 	   
-	public static void PrimePrinter (int x, int b)
-    {   
-		{while (b>=1 && b<=10)
-		System.out.println (isPrime);
-		}
-		{while (b>=10 && b<=20)
-	     System.out.println((isPrime);
-		}
-		{while b>=20 && b<=30
-		}
-		
+	public static void PrimePrinter (int x, int i,int count)
+    {isPrime (x); isPrime (i);   
+     if (count<=10)
+    		 {System.out.println(isPrime);}
+     if (count>10 && count <=20)
+             {System.out.println (isPrime);}
+     if (count >20 && count <=30)
+             {System.out.println(isPrime);}
+     if (count>30 && count <=40)
+             {System.out.println(isPrime);}
+     if (count>40 && count<=50)
+             {System.out.println (isPrime);}
+	 return;
 	}
 	
 	public static boolean isPrime(int x)
@@ -33,22 +35,34 @@ public class ControlStructures {
     }
 			
     }
-	public static int PrimeCount (int b)
-	{while (b>=1 && b<=10)
-	 {return b;}
+	public static boolean isPrime (int i)
+	{int number;
+	 boolean x= isPrime (x);
+	 if (!x)
+	 {return number;}
+			 
 	}
-		
+	
+	public static void foorBarBazPrint(int j,int count)
+	{fooBarBaz (a);
+	 if count <=500
+			 {System.out.println(fooBarBaz);}
+    }
+	
 	
     public static String fooBarBaz (int a)
     { for (int a1=0;a1<=500;a1++)
       {String printout=""; 
+       String foo=printout+"foo"
+       String Bar=printout+"Bar"
+       String Baz=printout+"Baz"
       if (a1%3==0)
-      {return printout+"foo";}
+      {return foo;}
       if (a1%7==0)
-      {return printout+"Bar";}
+      {return Bar;}
       if (a1%10==0)
-      {return printout+"Baz";}
-      {System.out.println(printout);}    
+      {return Baz;}
+      
     }
     	
 		   
